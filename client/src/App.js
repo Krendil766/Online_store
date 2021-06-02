2a9f0e805334d8dcf18887f425881da7576f575c
krendil766 <krendilek766@gmail.com>
2021-06-02 17:53:55 +0300

    feat:start mobx

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import AppRouter from "./components/AppRouter";
+
 const App = () => {
   return (
     <Router>
--- client/src/components/AppRouter.js
+++ client/src/components/AppRouter.js
@@ -1,21 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
-import { Auth, Shop } from "../pages";
 import { authRoutes, publickRoutes } from "../routes";
 import { TYPE_ROUTER } from "../utils/const";
+import { Context } from "../index";
 
 const AppRouter = () => {
-  const isAuth = false;
+  const {user} = useContext(Context);
   return (
     <Switch>
-      {isAuth &&
+      {user.isAuth &&
         authRoutes.map(({ path, component }) => {
           return <Route key={path} path={path} component={component} exact />;
         })}
       {publickRoutes.map(({ path, component }) => {
         return <Route key={path} path={path} component={component} exact />;
       })}
-      <Route render={() => <h2>Page not found</h2>} />
+      {/* <Route render={() => <h2>Page not found</h2>} /> */}
+      <Redirect to={TYPE_ROUTER.SHOP_ROUTER} />
     </Switch>
   );
 };
